81cc112158437b27e5fe0967567796a632ee7dfc
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-24 17:46:46 +0800

    refactor: remove global and local variables, update root element structure

--- to do list app/main.js
+++ to do list app/main.js
@@ -1,18 +1,8 @@
-const global = document.getElementById("global");
-const local = document.getElementById("local");
+ let rootDiv = document.getElementById("root").textContent="asdasd";
 
 
+ rootDiv.addEventListener("click", ()=>{
+  rootDiv = "change text";
+  console.log(rootDiv);
+ });
 
-let name = "Global Name"; // global variable
-
-function greet() {
-  let name = "Local Name"; // local variable shadows global 
-  return name;
-}
-
-greet();
-console.log(name); // prints: Global Name
-
- 
-global.innerText = name;
-local.innerText = greet();
