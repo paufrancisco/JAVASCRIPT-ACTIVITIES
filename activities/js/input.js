e1a6634bf7e53c831478146905d7be6496924148
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-26 11:38:42 +0800

    fix: correct baseText assignment to use h1Element for consistency

--- activities/js/input.js
+++ activities/js/input.js
@@ -1,6 +1,6 @@
 document.getElementById('mySubmit').onclick = function () {
     let h1Element = document.getElementById('h1');
-    let baseText = h1Element.textContent.split(' ')[0];  
+    let baseText = document.getElementById('h1').textContent.split(' ')[0]; 
 
     let userName = document.getElementById('myText').value;
 
