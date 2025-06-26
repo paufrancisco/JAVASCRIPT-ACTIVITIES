e774fdddd832b3930274887cc403ea4350a39958
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-26 10:39:32 +0800

    fix: update initial student count and correct population check logic

--- activities/js/operators.js
+++ activities/js/operators.js
@@ -1,15 +1,17 @@
-let students = 30;
+let students = 31;
 
 // students = students + 1;
 // students = students - 1;
 // students = students * 2;
 // students = students / 2;
-// students = students ** 2;
- 
-if (students%2==0){
-    console.log(`students population is even`);
-}else{
-    console.log(`studens population is odd`);
-}
+// students = students ** 2; 
+do{
+    if (students%2==0){
+        console.log(`students population is even`);  
+    }else{
+        console.log(`studens population is odd`);  
+    }
+    students++;
+}while(students<60);
 
 console.log(students);
