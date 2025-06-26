0897e5b8ff78d7baf253ba5e57f44a04d6dd0c2d
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-27 00:54:01 +0800

    fix: convert age input to number for accurate age verification in if-statements.js

--- activities/js/if-statements.js
+++ activities/js/if-statements.js
@@ -3,7 +3,8 @@ button.onclick = function(){
     let age = document.getElementById('myText'); 
     
     let result = document.getElementById('result');
-    age = age.value;
+    age = Number(age.value);
+    console.log(typeof age);
     if(age>=18){
         result.textContent = "Access approved";
     }else{
