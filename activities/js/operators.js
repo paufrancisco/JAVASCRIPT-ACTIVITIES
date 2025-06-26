d3515eaa62164472cb78947654eedeaeb9dc276a
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-26 10:33:27 +0800

    feat: add operators.js for student population calculations and update script reference in index.html

---
+++ activities/js/operators.js
@@ -0,0 +1,15 @@
+let students = 30;
+
+// students = students + 1;
+// students = students - 1;
+// students = students * 2;
+// students = students / 2;
+// students = students ** 2;
+ 
+if (students%2==0){
+    console.log(`students population is even`);
+}else{
+    console.log(`studens population is odd`);
+}
+
+console.log(students);
