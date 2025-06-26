c8fcc33889bf30cd5922d862c8ffc29f094c60b8
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-26 11:33:06 +0800

    feat: implement username input and update welcome message on submit

--- activities/js/input.js
+++ activities/js/input.js
@@ -1,4 +1,11 @@
-let username;
-username = window.prompt("What is your useranme?");
-document.getElementById('welcome').textContent+=username;
-console.log(username);
+document.getElementById('mySubmit').onclick = function () {
+    let h1Element = document.getElementById('h1');
+    let baseText = h1Element.textContent.split(' ')[0];  
+
+    let userName = document.getElementById('myText').value;
+
+ 
+    h1Element.textContent = baseText + " " + userName;
+
+    document.getElementById('myText').value = "";  
+}
