81acb86713f75d523f74377dc19913d1623e227a
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-27 10:00:34 +0800

    feat: add string method functionality in string-method.js and update script references in checkbox.html

---
+++ activities/js/string-method.js
@@ -0,0 +1,28 @@
+let userName = "Paulonism";
+let phoneNumber = "123-123-123";
+
+
+let chartAt = userName.charAt(0);
+let length = userName.length;
+let indexOf = userName.indexOf("u");
+let trim = userName.trim();
+let upperCase = userName.toUpperCase();
+let lowerCase = userName.toLowerCase();
+let repeat = userName.repeat(2);
+let startsWith = userName.startsWith("P");
+let endsWith = userName.endsWith("x");
+let replaceAll = phoneNumber.replaceAll("-","");
+let paddStart = phoneNumber.padStart(15,"X");
+
+
+console.log(chartAt);
+console.log(length);
+console.log(indexOf);
+console.log(trim);
+console.log(upperCase);
+console.log(lowerCase);
+console.log(repeat);
+console.log(startsWith);
+console.log(endsWith);
+console.log(replaceAll);
+console.log(paddStart);
