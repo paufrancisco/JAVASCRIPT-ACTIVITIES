0c26da38db24014d249c1e8fca410e6827e59d5a
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-27 09:43:37 +0800

    feat: add switch statement functionality in switch.js and include script reference in checkbox.html

---
+++ activities/js/switch.js
@@ -0,0 +1,28 @@
+let day = 2;
+
+switch (day){
+    case 1: 
+        console.log(`It is monday`);
+        break;
+    case 2:
+        console.log(`It is tuesday`); 
+        break;
+    case 3:
+        console.log(`It is wednesday`); 
+        break;
+    case 4:
+        console.log(`It is thursday`); 
+        break;
+    case 5:
+        console.log(`It is friday`); 
+        break;
+    case 6:
+        console.log(`It is saturday`); 
+        break;
+    case 7:
+        console.log(`It is sunday`); 
+        break;
+
+    default:
+        console.log(`Not a day`); 
+}
