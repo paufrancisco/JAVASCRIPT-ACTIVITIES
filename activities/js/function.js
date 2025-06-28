f97fab2207b98223e49e5e1e5ac6e849f96dbf6c
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-28 12:23:34 +0800

    fix: correct name capitalization and refactor happybirthday function to accept parameters

--- activities/js/function.js
+++ activities/js/function.js
@@ -1,9 +1,10 @@
-let name1 = 'paulo';
+let name1 = 'Paulo';
 let name2 = 'paulo1';
+let age = 29;
 let grades = [1,2,3,4,5];
 
 let sum =0;
-function happybirthday(grades){
+function getTheSum(grades){
   
     for(let i in grades){
         sum+=grades[i];
@@ -11,9 +12,12 @@ function happybirthday(grades){
     return sum; 
 }
 
-happybirthday(grades);
+function happybirthday(name,age){
+    console.log(`Happy birthday ${name}`);
+    console.log(`You are now ${age}`);
+}
+
+getTheSum(grades);
 console.log(sum);
 
-happybirthday(name2);
-happybirthday(name2);
-happybirthday(name2);
+happybirthday(name1,age);
