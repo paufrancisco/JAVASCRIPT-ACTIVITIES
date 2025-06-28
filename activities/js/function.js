7ef632dd9a69cf82ae6294f3f67e22370676eced
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-28 13:00:51 +0800

    fix: correct name capitalization in happybirthday function and update variable usage

--- activities/js/function.js
+++ activities/js/function.js
@@ -1,23 +1,55 @@
-let name1 = 'Paulo';
-let name2 = 'paulo1';
+let name = 'paulo';
+let email = `francisco.paulo.pf@gmail.com`;
+let num = 24;
+name = name[0].toUpperCase() + name.slice(1);
+
 let age = 29;
 let grades = [1,2,3,4,5];
 
-let sum =0;
+let sum = 0;
+
 function getTheSum(grades){
   
     for(let i in grades){
         sum+=grades[i];
     } 
+    console.log(`Grades: ${grades}`);
     return sum; 
 }
 
+
 function happybirthday(name,age){
-    console.log(`Happy birthday ${name}`);
-    console.log(`You are now ${age}`);
+    console.log(`Happy Birthday ${name}!`);
+    console.log(`Age: ${age}`);
 }
 
-getTheSum(grades);
-console.log(sum);
+function evenoddfilter(num){
+    if(num % 2 == 0){
+        console.log(`Even: ${num}`);
+    }else{
+        console.log(`Odd: ${num}`);
+    }
+}
 
-happybirthday(name1,age);
+
+function isValidEmail(email){
+    if(email.includes("@")){
+        console.log(`${email} is valid`);
+    }else{
+        console.log(`${email} is invalid`);
+    }
+}
+
+
+let sumWithkeme = getTheSum(grades);
+
+console.log(`Grades with keme: ${sumWithkeme}`);
+
+
+getTheSum(grades); 
+
+happybirthday(name,age);
+
+evenoddfilter(num);
+
+isValidEmail(email);
