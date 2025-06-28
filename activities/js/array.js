6a69b371524d622353daa11920e478f6d2e1149e
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-28 19:39:21 +0800

    fix: add input validation for fruit addition in the list

--- activities/js/array.js
+++ activities/js/array.js
@@ -7,8 +7,22 @@ let fruits = [];
 
 btnAdd.onclick = function(){
     newFruit = document.getElementById('add').value; 
-    fruits.push(newFruit); 
-    fruitList.innerHTML = `List of fruits: ${fruits}<br> No. of fruits : ${fruits.length}`;
-    document.getElementById('add').value = '';
+    if (newFruit!==""){
+        fruits.push(newFruit); 
+        fruitList.innerHTML = `List of fruits: ${fruits}<br> No. of fruits : ${fruits.length}`;
+        document.getElementById('add').value = '';
+        let index = fruits.indexOf("apple");
+        console.log(index);
+    }else{
+        fruitList.innerHTML = `Please enter fruit`; 
+    }
+    
 }
 
+
+
+let hardwares = ['mouse','keyboard','monitor'];
+
+for(i in hardwares){
+    console.log(hardwares[i]);
+}
