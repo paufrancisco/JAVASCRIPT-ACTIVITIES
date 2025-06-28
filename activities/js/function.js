5db8aa2055b06c7a63000ed4997e6697cb8ffe3b
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-28 12:10:38 +0800

    feat: refactor happybirthday function to accept grades and log sum

--- activities/js/function.js
+++ activities/js/function.js
@@ -1,8 +1,19 @@
-function happybirthday(){
-    console.log('happy birthday');
+let name1 = 'paulo';
+let name2 = 'paulo1';
+let grades = [1,2,3,4,5];
+
+let sum =0;
+function happybirthday(grades){
+  
+    for(let i in grades){
+        sum+=grades[i];
+    } 
+    return sum; 
 }
 
-happybirthday();
-happybirthday();
-happybirthday();
-happybirthday();
+happybirthday(grades);
+console.log(sum);
+
+happybirthday(name2);
+happybirthday(name2);
+happybirthday(name2);
