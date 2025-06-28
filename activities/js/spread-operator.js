a92d32eadb0679b2f503962f151835dd2ef1a1c6
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-28 21:54:18 +0800

    fix: update sample array and improve max logging in spread-operator.js

--- activities/js/spread-operator.js
+++ activities/js/spread-operator.js
@@ -7,14 +7,14 @@ console.log(maximum);
 console.log(minimum);
 
 
-let sample = [4, 12, 8, 210, 5];
+let sample = [12,2,3,4,5,6];
 let max = sample[0];
 
 for(let i = 0; i<sample.length; i++){
-    if(sample[i] > max){
+    if(sample[i]>max){
         max = sample[i];
-        console.log(`This is the max: ${max}`);
+        console.log(`New max: ${max}`);
     }
-    console.log(sample[i]);
 }
-console.log(`Final max ${max}`);
+
+console.log(`Final max: ${max}`);
