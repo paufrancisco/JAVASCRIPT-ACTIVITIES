b100bf4788b1e086ff49180d96b6b9ac68a944ae
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-28 21:50:51 +0800

    fix: update title in spread-operator.html and enhance logging in spread-operator.js

--- activities/js/spread-operator.js
+++ activities/js/spread-operator.js
@@ -5,3 +5,16 @@ let minimum = Math.min(...numbers);
 
 console.log(maximum); 
 console.log(minimum);
+
+
+let sample = [4, 12, 8, 210, 5];
+let max = sample[0];
+
+for(let i = 0; i<sample.length; i++){
+    if(sample[i] > max){
+        max = sample[i];
+        console.log(`This is the max: ${max}`);
+    }
+    console.log(sample[i]);
+}
+console.log(`Final max ${max}`);
