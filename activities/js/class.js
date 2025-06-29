7c068493d1c7b07b821d8b6f8e575a2b327a2fe8
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 06:28:22 +0800

    feat: update Product class to include quantity and adjust total calculation

--- activities/js/class.js
+++ activities/js/class.js
@@ -19,17 +19,48 @@
 // product1.displayProduct();
 // console.log(`Total with tax: ${product1.calculateTotal().toFixed(2)}`);
 
-function Product(name, price){
+function Product(name, price, qty){
     this.name = name;
     this.price = price;
+    this.qty = qty;
 
-    this.CalculateTotal = function(){ 
-        return (this.price - this.price*salesTax).toFixed(2);
+    this.calculateTotal = function(){ 
+        return ((this.price - this.price*salesTax)*this.qty).toFixed(2);
     };
+
+    this.displayProduct = function(){
+        console.log(`The product: ${this.name} cost ${this.price}`);
+    }; 
+      
 }
 
-const salesTax = 0.05;
-const product = new Product("mema",12);
+function Student(name,age,section,id){
+    this.name = name;
+    this.age = age;
+    this.section = section;
+    this.id = id;
 
-let total = product.CalculateTotal();
+    this.displayStudentInfo = function(){
+        console.log(`Student #${this.id}: ${this.name} ${this.age} years old. Section ${this.section}`);
+    }
+
+
+}
+
+const student = new Student(
+    "name",
+    24,
+    "Rizal",
+    287724
+);
+
+
+student.displayStudentInfo();
+
+ 
+
+const salesTax = 0.05;
+const product = new Product("Mikmik",12,200); 
+let total = product.calculateTotal();
+product.displayProduct();
 console.log(total);
