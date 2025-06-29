2fa1f2e8525c50d19b8c1b01773dcd108af63905
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-29 12:40:42 +0800

    feat: enhance maps.js with additional mapping for numbers and fruits

--- activities/js/maps.js
+++ activities/js/maps.js
@@ -1,10 +1,11 @@
 const numbers = [1,2,3,4,5];
 const squares = numbers.map(square);
 const cube = numbers.map(numberCube);
+const withLetter = numbers.map(addLetter);
 
 console.log(squares, typeof squares);
 console.log(cube, typeof cube);
-
+console.log(withLetter, typeof withLetter);
 
 function square(element){
     return Math.pow(element, 2);
@@ -16,5 +17,22 @@ function numberCube(element){
 }
 
 function addLetter(element){
-    return element+"Mema";
+    return element*3 + "Mema";
+}
+
+
+
+
+const fruits = ["Banana", "Apple", "Orange"];
+const withLabel = fruits.map(addSomething);
+
+displayFruits(withLabel);
+
+
+function addSomething(element){
+    return "Fruits: " + element;
+}
+
+function displayFruits(element){
+    console.log(element);
 }
