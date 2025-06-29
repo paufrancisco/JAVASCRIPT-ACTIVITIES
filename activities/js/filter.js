cc32c4e862835c77e4cf6fab8355a862355e7732
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-29 17:42:05 +0800

    refactor: comment out unused filtering functions and variables in filter.js

--- activities/js/filter.js
+++ activities/js/filter.js
@@ -1,29 +1,45 @@
-let numbers = [1,2,3,4,5,6,7,8,9,10];
-let even = numbers.filter(isEven);
-let odd = numbers.filter(isOdd);
+// let numbers = [1,2,3,4,5,6,7,8,9,10];
+// let even = numbers.filter(isEven);
+// let odd = numbers.filter(isOdd);
 
-function isEven(element){
-    return element % 2 === 0;
-}
+// function isEven(element){
+//     return element % 2 === 0;
+// }
 
-function isOdd(element){
-    return element % 3 === 0;
-}
+// function isOdd(element){
+//     return element % 3 === 0;
+// }
 
-console.log(even);
-console.log(odd);
+// console.log(even);
+// console.log(odd);
  
 
-const words = ["apple", "orange", "bananas", "coconut", "123456"];
-const sixLetter = words.filter(filter6words);
+// const words = ["apple", "orange", "bananas", "coconut", "123456"];
+// const sixLetter = words.filter(filter6words);
 
-function filter6words(element){
-    return element.length < 6 ? true : false;
+// function filter6words(element){
+//     return element.length < 6 ? true : false;
+// }
+
+
+// console.log(sixLetter);
+
+// const prices = [1,2,3,4,5];
+
+// const total = prices.reduce(sum);
+
+// console.log(`${total}`);
+
+// function sum(accumulator, element){ 
+//     return accumulator + element;
+// } 
+
+
+const grades = [75,87,87,89,76,90];
+
+const maximum = grades.reduce(getMax);
+
+function getMax(accumulator, element){
+    return Math.max(accumulator, element);
 }
-
-
-console.log(sixLetter);
-
-const prices = [213.45,23,423];
-
-
+console.log(maximum);   
