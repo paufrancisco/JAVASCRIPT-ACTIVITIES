efa8507d8ba0d00eaff2bb73456174b2807693c2
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-29 19:10:57 +0800

    feat: add object.js with person object and methods, update script source in filter.html

---
+++ activities/js/object.js
@@ -0,0 +1,12 @@
+const person = {
+    firstName: "Spongebob",
+    lastName: "Squarepants",
+    age: 30,
+    isEmployed: true,
+    sayBye: function(){console.log("Bye")},
+}
+
+
+console.log(person.firstName);
+console.log(person.lastName);
+person.sayBye();
