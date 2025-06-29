d30e3c3e8bc26b51b1044dbd002f777ffe67c43e
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-29 20:06:20 +0800

    feat: add car2 instance to constructor.js and log its properties

--- activities/js/constructor.js
+++ activities/js/constructor.js
@@ -10,9 +10,19 @@ const car1 = new Car("Ford",
                      "2024",
                      "Black"  
 )
+const car2 = new Car("Chevrolet",
+                     "Camaro",
+                     "2025",
+                     "Blue"  
+)
+
 
 console.log(car1.make);
 console.log(car1.model);
 console.log(car1.year);
 console.log(car1.color);
 
+console.log(car2.make);
+console.log(car2.model);
+console.log(car2.year);
+console.log(car2.color);
