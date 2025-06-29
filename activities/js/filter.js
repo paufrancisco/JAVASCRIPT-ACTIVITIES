52544a9776f003b8033980a759c87fe28e159493
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-29 17:43:29 +0800

    feat: add minimum grade calculation to filter.js

--- activities/js/filter.js
+++ activities/js/filter.js
@@ -38,8 +38,15 @@
 const grades = [75,87,87,89,76,90];
 
 const maximum = grades.reduce(getMax);
+const minimum = grades.reduce(getMin);
 
 function getMax(accumulator, element){
     return Math.max(accumulator, element);
 }
+function getMin(accumulator, element){
+    return Math.min(accumulator, element);
+}
+
+ 
 console.log(maximum);   
+console.log(minimum);   
