fae428ea243d430b24f733a9665164eedef9c38b
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 15:36:05 +0800

    feat: replace shuffle script with date script in class.html and add date.js file

---
+++ activities/js/date.js
@@ -0,0 +1,12 @@
+const date = new Date();
+
+const year = date.getFullYear();
+const month = date.getMonth(); 
+const day = date.getDate();
+
+
+console.log(date);
+ 
+console.log(`Years: ${year}`); 
+console.log(`Month: ${month}`);
+console.log(`Day: ${day}`);
