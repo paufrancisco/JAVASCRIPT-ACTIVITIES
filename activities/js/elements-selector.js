2ba72bb605910fb4936271a42f7b8419c5df0e01
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 23:52:40 +0800

    feat: enhance filter.html structure and update elements-selector.js for improved styling

--- activities/js/elements-selector.js
+++ activities/js/elements-selector.js
@@ -4,3 +4,28 @@ myHeadings.style.backgroundColor = "red";
 myHeadings.style.textAlign = "center";
 
 console.log(myHeadings);
+
+const myFruits = document.getElementsByClassName('fruits');
+
+// for (let i in myFruits){
+//     myFruits[i].style.backgroundColor = "yellow";
+// }
+console.log(myFruits); 
+
+Array.from(myFruits).forEach(myFruits =>{
+    myFruits.style.backgroundColor = "blue";
+})
+
+const h4Elements = document.getElementsByTagName("h4");
+const liElements = document.getElementsByTagName("li");
+
+for(let h4element of h4Elements){
+    h4element.style.backgroundColor = "yellow";
+}
+
+for(let liElement of liElements){
+    liElement.style.backgroundColor = "blue";
+}
+
+
+console.log(h4Elements);
