4c4cdce926adacff2d4b448032e3572253c7227d
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 08:45:44 +0800

    feat: implement movement methods for Animal subclasses and enhance functionality

--- activities/js/super.js
+++ activities/js/super.js
@@ -3,6 +3,9 @@ class Animal {
         this.name = name;
         this.age = age;
     }
+    move(speed){
+        console.log(`The ${this.name} moves at a speed of ${speed}mph`);
+    }
 }
 
 class Rabbit extends Animal {
@@ -10,6 +13,11 @@ class Rabbit extends Animal {
     constructor(name, age, runSpeed){
         super(name, age); 
         this.runSpeed = runSpeed;
+    }
+
+    run(){
+        console.log(`This ${this.name} can run`);
+        super.move(this.runSpeed);
     }
     
 }
@@ -20,6 +28,10 @@ class Fish extends Animal {
         super(name, age);  
         this.swimSpeed = swimSpeed;
     }
+    swim(){
+        console.log(`This ${this.name} can swim`);
+        super.move(this.swimSpeed);
+    }
 
 }
 
@@ -29,13 +41,26 @@ class Hawk extends Animal {
         super(name,age); 
         this.flySpeed = flySpeed;
     }
+    fly(){
+        console.log(`This ${this.name} can fly`);
+        super.move(this.flySpeed);
+    }
 
 }
+ 
 
 const rabbit = new Rabbit("Rabbit", 24, 25);
 const fish = new Fish("Fish", 2, 12);
 const hawk = new Hawk("Hawk", 3, 50);
 
+const animal = new Animal("asd",23);
+
 console.log(rabbit);
 console.log(fish);
 console.log(hawk);
+
+fish.swim();
+rabbit.run();
+hawk.fly();
+
+animal.move();
