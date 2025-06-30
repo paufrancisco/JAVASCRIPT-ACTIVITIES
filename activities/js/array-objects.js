086eff7e3388f183849c2853286496161fcbc9a1
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 14:11:24 +0800

    feat: update class.html to reference array-objects.js and add array-objects.js file

---
+++ activities/js/array-objects.js
@@ -0,0 +1,8 @@
+const fruits = [
+  { name: "apple", color: "red" },
+  { name: "banana", color: "yellow" },
+  { name: "grape", color: "purple" },
+  { name: "orange", color: "orange" }
+];
+fruits.push({name:"mema", color: "mema"} );
+console.log(fruits[4].name,fruits[4].color);
