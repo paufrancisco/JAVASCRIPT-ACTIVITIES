a8626e1f2040c15f4dc33e3fd1635a56ad0ec4c8
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 15:59:06 +0800

    feat: update date.js to include hour, minute, and second logging; replace date.js script with closure.js in class.html

---
+++ activities/js/closure.js
@@ -0,0 +1,27 @@
+function createCounter(){
+    let count = 0;
+    message = "Goodbye"; 
+
+    function increment(){
+        
+        count ++;
+        console.log(`Count: ${count}`);
+    }
+     
+    function getCount(){
+        return count;
+    }
+
+    return {increment,getCount};
+}
+
+ 
+const counter = createCounter();
+
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment();
+counter.increment(); 
+
+console.log(`The current count is ${counter.getCount()}`);
--- activities/js/date.js
+++ activities/js/date.js
@@ -3,10 +3,15 @@ const date = new Date();
 const year = date.getFullYear();
 const month = date.getMonth(); 
 const day = date.getDate();
-
+const hour = date.getHours();
+const min = date.getMinutes();
+const sec = date.getSeconds();
 
 console.log(date);
  
 console.log(`Years: ${year}`); 
 console.log(`Month: ${month}`);
 console.log(`Day: ${day}`);
+console.log(`Hours: ${hour}`);
+console.log(`Minutes: ${min}`);
+console.log(`Seconds: ${sec}`);
