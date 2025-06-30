f0b6944f543eee9896f7cb6e5cd1895abd4b8d8f
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 14:20:46 +0800

    feat: uncomment console logs and clean up fruit iteration methods in array-objects.js

--- activities/js/array-objects.js
+++ activities/js/array-objects.js
@@ -5,8 +5,14 @@ const fruits = [
   { name: "orange", color: "orange" }
 ];
 fruits.push({name:"mema", color: "mema"} );
-console.log(fruits[4].name,fruits[4].color);
+// console.log(fruits[4].name,fruits[4].color);
 
-for( i in fruits){
-    console.log(fruits[i]);
-}
+// for( i in fruits){
+//     console.log(fruits[i]);
+// }
+// fruits.forEach(fruits => console.log(fruits.name));
+
+const fruitNames = fruits.map(fruits => fruits.name);
+const fruitColors = fruits.map(fruits => fruits.color);
+console.log(fruitNames);
+console.log(fruitColors);
