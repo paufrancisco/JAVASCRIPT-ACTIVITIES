a8b0dadd02e78dda3888a10f0f6d7972ac84e701
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 14:55:04 +0800

    feat: clean up array-objects.js by commenting out unused code and add sorting example

--- activities/js/array-objects.js
+++ activities/js/array-objects.js
@@ -12,7 +12,16 @@ fruits.push({name:"mema", color: "mema"} );
 // }
 // fruits.forEach(fruits => console.log(fruits.name));
 
-const fruitNames = fruits.map(fruits => fruits.name);
-const fruitColors = fruits.map(fruits => fruits.color);
-console.log(fruitNames);
-console.log(fruitColors);
+// const fruitNames = fruits.map(fruits => fruits.name);
+// const fruitColors = fruits.map(fruits => fruits.color);
+
+// // console.log(fruitNames);
+// // console.log(fruitColors);
+
+// let fruit = ["apple", "orange", "banana", "coconut"];
+// fruit.sort();
+// console.log(fruit);
+
+let numbers = [11,11,3,4,5];
+numbers.sort((a,b) =>console.log(`this is ${a}`,`this is ${b}`,a-b));
+console.log(numbers);
