4cf0f96f3cdf41d181f1ca07c4a98981d80e0691
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 17:57:11 +0800

    feat: format output of circumference, area, and volume calculations to two decimal places

--- activities/js/es6.js
+++ activities/js/es6.js
@@ -1,6 +1,6 @@
 import {PI, getCircumference, getArea, getVolume} from './mathUtil.js';
 
 console.log(PI);
-console.log(getCircumference(2));
-console.log(getArea(2));
-console.log(getVolume(2));
+console.log(getCircumference(2).toFixed(2));
+console.log(getArea(2).toFixed(2));
+console.log(getVolume(2).toFixed(2));
