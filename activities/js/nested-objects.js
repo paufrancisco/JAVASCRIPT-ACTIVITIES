8cfd94a7bc4fe5c0aae9d738b12762ba1fb5bd7e
paufrancisco <francisco.paulo.pf@gmail.com>
2025-06-30 13:54:19 +0800

    feat: update class.html to reference nested-objects.js and add nested-objects.js file

---
+++ activities/js/nested-objects.js
@@ -0,0 +1,14 @@
+const person = {
+
+    fullName: "Paulo Francisco",
+    age: 29,
+    hobbies: ["Chess", "Basketball"],
+    address: {
+        street: "Narra St.",
+        city: "Dasmarinas",
+        country: "Philippines"
+    }
+}
+
+console.log(person.address.city);
+console.log(person.hobbies[0]);
