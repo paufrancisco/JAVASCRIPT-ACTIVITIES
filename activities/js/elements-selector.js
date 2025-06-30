20405d333e274e42b9e858138b59aa41c9c6607f
paufrancisco <francisco.paulo.pf@gmail.com>
2025-07-01 00:00:16 +0800

    feat: comment out unused code in elements-selector.js for cleaner structure

--- activities/js/elements-selector.js
+++ activities/js/elements-selector.js
@@ -1,31 +1,44 @@
-const myHeadings = document.getElementById('my-heading');
+// const myHeadings = document.getElementById('my-heading');
 
-myHeadings.style.backgroundColor = "red";
-myHeadings.style.textAlign = "center";
+// myHeadings.style.backgroundColor = "red";
+// myHeadings.style.textAlign = "center";
 
-console.log(myHeadings);
+// console.log(myHeadings);
 
-const myFruits = document.getElementsByClassName('fruits');
+// const myFruits = document.getElementsByClassName('fruits');
 
-// for (let i in myFruits){
-//     myFruits[i].style.backgroundColor = "yellow";
+// // for (let i in myFruits){
+// //     myFruits[i].style.backgroundColor = "yellow";
+// // }
+// console.log(myFruits); 
+
+// Array.from(myFruits).forEach(myFruits =>{
+//     myFruits.style.backgroundColor = "blue";
+// })
+
+// const h4Elements = document.getElementsByTagName("h4");
+// const liElements = document.getElementsByTagName("li");
+
+// for(let h4element of h4Elements){
+//     h4element.style.backgroundColor = "yellow";
 // }
-console.log(myFruits); 
 
-Array.from(myFruits).forEach(myFruits =>{
-    myFruits.style.backgroundColor = "blue";
+// for(let liElement of liElements){
+//     liElement.style.backgroundColor = "lightgreen";
+// }
+
+
+// console.log(h4Elements);
+
+// const element = document.querySelector(".fruits");
+
+// element.style.backgroundColor = "red";
+
+
+const fruits = document.querySelectorAll("li");
+
+fruits.forEach(foods =>{
+    foods.style.backgroundColor = "yellow";
 })
 
-const h4Elements = document.getElementsByTagName("h4");
-const liElements = document.getElementsByTagName("li");
-
-for(let h4element of h4Elements){
-    h4element.style.backgroundColor = "yellow";
-}
-
-for(let liElement of liElements){
-    liElement.style.backgroundColor = "blue";
-}
-
-
-console.log(h4Elements);
+console.log(foods);
