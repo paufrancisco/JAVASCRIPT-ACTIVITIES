469b2b9d9fc70fb07944cc89e5c71baa39d1cca8
paufrancisco <francisco.paulo.pf@gmail.com>
2025-07-05 01:51:47 +0800

    feat: fix syntax errors in click-events.js and add nodelist.html and nodelist.js files

--- activities/js/click-events.js
+++ activities/js/click-events.js
@@ -8,8 +8,8 @@ myBtn.addEventListener("click", function changeColor(event){
 
 myBtn.addEventListener("mouseover", event =>{
     event.target.style.backgroundColor = "blue";
-})
+});
 
 myBtn.addEventListener("mouseleave", event =>{
     event.target.style.backgroundColor = "green";
-})
+});
