553ed8d4e0ddacfe53ef0937d4f4cff7968867e3
paufrancisco <francisco.paulo.pf@gmail.com>
2025-07-05 13:42:59 +0800

    feat: add disabled state styling and update button click behavior in classList.js

--- activities/js/classList.js
+++ activities/js/classList.js
@@ -1,8 +1,19 @@
 const myButton = document.getElementById('myButton');
 
-myButton.classList.add("enabled");
-myButton.classList.remove("enabled");
+// myButton.classList.add("enabled");
+// myButton.classList.remove("enabled");
 
-myButton.addEventListener("mouseover", event=>{
-    event.target.classList.add('hover');
+// myButton.addEventListener("mouseover", event=>{
+//     event.target.classList.add('hover');
+// })
+
+// myButton.addEventListener("mouseout", event=>{
+//     event.target.classList.remove('hover');
+// })
+
+myButton.classList.add('enabled');
+
+myButton.addEventListener("click", event=>{
+    event.target.classList.replace('enabled','disabled');
 })
+
