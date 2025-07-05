1dfba24281c7cdfdf0c1bed108e173f75eaf05f4
paufrancisco <francisco.paulo.pf@gmail.com>
2025-07-06 00:57:42 +0800

    feat: implement slider functionality with image display and navigation buttons

---
+++ activities/js/slider.js
@@ -0,0 +1,22 @@
+const slides = document.querySelectorAll('.slides img');
+let slideIndex = 0;
+let interValid = null;
+
+initializeSlider()
+function initializeSlider(){
+    slides[slideIndex].classList.add("displaySlide");
+}
+
+function showSlide(index){
+
+}
+
+
+
+function prevSlide(){
+
+}
+
+function nextSlide(){
+    
+}
