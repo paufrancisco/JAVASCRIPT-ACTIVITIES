4186aed875af294912ad5a15b4fdbb21a1f75c75
paufrancisco <francisco.paulo.pf@gmail.com>
2025-07-05 14:00:16 +0800

    refactor: clean up classList.html and classList.js, remove unused elements and enhance button hover effect

--- activities/js/classList.js
+++ activities/js/classList.js
@@ -1,36 +1,47 @@
-const myButton = document.getElementById('myButton');
-const myH1 = document.getElementById('h1');
-// myButton.classList.add("enabled");
-// myButton.classList.remove("enabled");
+ 
+// // myButton.classList.add("enabled");
+// // myButton.classList.remove("enabled");
 
-// myButton.addEventListener("mouseover", event=>{
-//     event.target.classList.add('hover');
-// })
+// // myButton.addEventListener("mouseover", event=>{
+// //     event.target.classList.add('hover');
+// // })
 
-// myButton.addEventListener("mouseout", event=>{
-//     event.target.classList.remove('hover');
-// })
-myH1.classList.add('enabled');
-myButton.classList.add('enabled');
+// // myButton.addEventListener("mouseout", event=>{
+// //     event.target.classList.remove('hover');
+// // })
+// myH1.classList.add('enabled');
+// myButton.classList.add('enabled');
 
-myButton.addEventListener("click", event=>{
+// myButton.addEventListener("click", event=>{
 
 
-    if(event.target.classList.contains('disabled')){
-        event.target.textContent += "mema";
-    }
-    else{
-        event.target.classList.replace('enabled','disabled');
-    } 
+//     if(event.target.classList.contains('disabled')){
+//         event.target.textContent += "mema";
+//     }
+//     else{
+//         event.target.classList.replace('enabled','disabled');
+//     } 
+// });
+
+// myH1.addEventListener("click", event=>{
+
+
+//     if(event.target.classList.contains('disabled')){
+//         event.target.textContent += "mema";
+//     }
+//     else{
+//         event.target.classList.replace('enabled','disabled');
+//     } 
+// });
+
+let buttons = document.querySelectorAll('.myButtons');
+
+buttons.forEach(button => {
+    button.classList.add("hover");
 });
 
-myH1.addEventListener("click", event=>{
-
-
-    if(event.target.classList.contains('disabled')){
-        event.target.textContent += "mema";
-    }
-    else{
-        event.target.classList.replace('enabled','disabled');
-    } 
-});
+buttons.forEach(button =>{
+    button.addEventListener("mouseover", event=>{
+        event.target.classList.toggle("hover");
+    })
+})
