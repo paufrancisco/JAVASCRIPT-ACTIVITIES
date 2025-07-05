47bb7d5869cd5d06696a01799edfb962fbb8f4af
paufrancisco <francisco.paulo.pf@gmail.com>
2025-07-05 22:04:19 +0800

    feat: enhance playGame function to determine game result and update displays

--- activities/js/rock-paper-scissors.js
+++ activities/js/rock-paper-scissors.js
@@ -3,3 +3,32 @@ const choices = ["rock", "paper","scissors"];
 const playerDisplay = document.getElementById('playerDisplay');
 const computerDisplay = document.getElementById('computerDisplay');
 const resultDisplay = document.getElementById('resultDisplay');
+
+function playGame(playerChoice){ 
+    const computerChoice = choices[Math.floor(Math.random()*3)];
+    console.log(computerChoice); 
+    
+    let result = "";
+    
+    if(playerChoice === computerChoice){
+       result = 'Its a tie';
+    }
+    else if
+    (playerChoice === 'paper' && computerChoice === 'rock'||
+     playerChoice === 'rock' && computerChoice === 'scissors'||
+     playerChoice === 'scissors' && computerChoice === 'paper'
+    ) 
+    {
+        result = 'you win';
+    }else{
+        result = 'you lose';
+    }
+
+    playerDisplay.textContent = `Player: ${playerChoice}`;
+    computerDisplay.textContent = `Computer: ${computerChoice}`;
+    resultDisplay.textContent = result;
+
+
+     
+}
+
